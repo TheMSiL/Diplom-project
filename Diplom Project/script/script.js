74a378ca0012ce73e7c6b90cf17b0e131375c8fd
Maks <108630818+TheMSiL@users.noreply.github.com>
2022-09-10 15:35:03 +0300

    Delete Diplom Project directory

--- Diplom Project/script/script.js
+++
@@ -1,5 +0,0 @@
-$('.vacancy_items > div') .on('click', function(){
-    $(this).children('.vacancy_items-icon').toggleClass('vacancy_items-icon__rotate');
-    $(this).parent('.vacancy_items').toggleClass('change-color')
-    $(this).next('.vacancy_items-text').slideToggle(300);
-})
